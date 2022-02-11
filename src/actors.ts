dab9832b8632309e63fbfa1c405dd376fb68ded3
Jeff Borders <jeff.borders@legacymusicpartners.com>
2022-02-11 11:51:59 -0600

    v1.1.0 updating actors and removing video uploads

--- src/actors.ts
+++ src/actors.ts
@@ -1,18 +1,15 @@
 const actors = {
-  'aleksandar-ilic-lmp': 'U01A7010YHW',
   'andrej-zentner-lmp': 'U019E27DKCM',
   'billy-fortin-coda': 'U015MLV41SL',
-  'bogdan-poplauschi-coda': 'U0183M9NMPU',
   'colton-savage-lmp': 'U014WEHJUTX',
   'jeff-borders-coda': 'U017GEJ0M0C',
-  'john-sylvain-coda': 'U0161MH110F',
   'meg-okeefe-coda': 'U015Y01C5HC',
   'mihai-rustiuc-coda': 'U01866TL2JV',
-  'mike-beirne-coda': 'U01B7URUW20',
   'mike-piacenza-lmp': 'U010HEQUN6B',
-  'vid-staric-lmp': 'U019H4RGK26',
   'yao-zhang-coda': 'U017ZPV3M44',
-  'zach-glazer-coda': 'U015Y017HTQ'
+  'keri-weiss-coda': 'U02MRRD9BFD',
+  'luis-solano-coda': 'U02K654CABH',
+  'vince-zipparro-coda': 'U02E70XGRFF'
 }
 
 export default actors
